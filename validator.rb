0e6026e7adbc222a6b9017c3dc26cc0a3e204239
Trevor John <trevor@john.tj>
2013-02-19 00:04:51 -0500

    Update validator

--- validator.rb
+++ validator.rb
@@ -1,41 +1,51 @@
 #!/usr/bin/ruby
 require 'curb'
 
-base_url = ".github.com/"
 queries = %w(index.html resume.html bio.html)
+
+base_url = ".github.com/"
 html_prefix = "http://validator.w3.org/check?uri="
 html_postfix = "&charset=%28detect+automatically%29&doctype=Inline&group=0"
 css_prefix = "http://jigsaw.w3.org/css-validator/validator?uri="
 css_postfix = "&profile=css3&usermedium=all&warning=1&vextwarning=&lang=en"
 
-ARGV.each do |username|
-  puts "*"*40 + " " + username + " " + "*"*40
-  puts username + ".github.com"
-  puts "github.com/#{username}/#{username}.github.com/"
+usernames = []
+html_failures = []
+css_failures = []
+errs = []
+
+usernames.each do |username|
+  puts "-"*15 + " " + username + " " + "-"*15
+
   queries.each do |q|
-    html = html_prefix + username + base_url + q + html_postfix
-    curl = Curl::Easy.new(html)
-    begin
-      curl.perform
-    rescue
-      assert(true, "#{username} - #{q} failed for html")
-    end
-    if curl.body_str.match('class="invalid">Errors')
+    url = html_prefix + username + base_url + q + html_postfix
+
+    response = `curl -v #{ url } --retry 3`
+
+    if response.empty?
+      puts "#{ username } did not respond"
+      errs << username
+    else reponse.match('class="invalid">Errors')
       puts "#{username} - Errors found for #{q} in HTML"
+      html_failures << [username, q].join(" - ")
     end
 
-    css = css_prefix + username + base_url + q + css_postfix
-    curl = Curl::Easy.new(css)
-    begin
-      curl.perform
-    rescue
-      assert(true, "#{q} failed for css")
-    end
-    if curl.body_str.match('id="errors"')
+    url = css_prefix + username + base_url + q + css_postfix
+
+    response = `curl -v #{ url } --retry 3`
+    if response.empty?
+      puts "#{ username } did not respond"
+      errs << username
+    else reponse.match('class="invalid">Errors')
       puts "#{username} - Errors found for #{q} in CSS"
+      css_failures << [username, q].join(" - ")
     end
   end
-  puts "="*80
 end
 
-exec 'osascript -e "beep 5"'
+puts "-"*15 + " Failures " + "-"*15
+html_failures.each { |f| puts f }
+css_failures.each { |f| puts f }
+
+puts "-"*15 + " Errors " + "-"*15
+errs.each { |e| puts e }
