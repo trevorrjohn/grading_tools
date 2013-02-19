617b8a6fc55bc1111b0affa853eb2aacb83d81d6
Trevor John <trevor@john.tj>
2013-02-18 23:45:24 -0500

    Update branch creator and remover

--- branch_remover.rb
+++ branch_remover.rb
@@ -1,15 +1,14 @@
 #!/usr/bin/ruby
 
 repos = %w()
-errs = %w()
+errs = []
 username = ""
 branch = ""
 dir = Dir.pwd
-
 repos.each do |repo|
   puts "-"*15 + " " + repo + " " + "-"*15
 
-  unless system "git clone https://github.com/#{ username }/#{ repo }.git repo && cd repo && git branch #{ branch } && git push origin #{ branch } && cd - && rm -rf repo"
+  unless system "git clone https://github.com/#{ username }/#{ repo }.git repo && cd repo && git push origin :#{ branch } && cd - && rm -rf repo"
     system "cd #{ dir } && rm -rf repo"
     errs << repo
   end
