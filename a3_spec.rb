587a40b2f0453e6cd772520e3eee578b35382ad9
Trevor John <trevor@john.tj>
2013-03-07 15:06:26 -0500

    Test for js errors

--- a3_spec.rb
+++ a3_spec.rb
@@ -3,15 +3,19 @@ require 'rspec'
 require 'capybara'
 require 'capybara/rspec'
 require 'capybara/dsl'
+require 'capybara-webkit'
 require './spec_helper.rb'
 
 Capybara.run_server = false
-Capybara.current_driver = :selenium
+Capybara.current_driver = :webkit
 Capybara.app_host = 'http://tuftsdev.github.com'
 
 describe "testing" do
   it "displays something" do
-    visit "/"
-    page.should have_content "something"
+    visit "//where/"
+
+    page.driver.console_messages.length.should == 0
+    page.driver.error_messages.length.should == 0
+
   end
 end
